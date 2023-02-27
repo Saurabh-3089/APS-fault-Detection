3a3dbad0476a2e72b6f35c1a2bb3e8e011d8fd28
Saurabh-3089 <saurabhchauhan3089@gmail.com>
2023-02-27 09:15:47 +0530

    Data Dumped in MongoDB

---
+++ data_dump.py
@@ -0,0 +1,25 @@
+import pymongo
+import pandas as pd
+import json
+
+# Provide the mongodb localhost url to connect python to mongodb.
+client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+
+FILE_PATH="/config/workspace/aps_failure_training_set1.csv"
+DATABASE_NAME="aps"
+COLLECTION_NAME="sensor"
+
+if __name__=="__main__":
+    df = pd.read_csv(FILE_PATH)
+    print(f"Rows and Columns: {df.shape}")
+
+    #convert df to json to dump csv to MongoDB
+    #Drop the index
+    df.reset_index(drop=True,inplace=True)
+
+    #json.loads convert json to python dictionary object
+    json_record=list(json.loads(df.T.to_json()).values())
+    #print(json_record[0])
+
+    #insert converted json record to mongo db
+    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
---
+++ setup.py
@@ -0,0 +1,8 @@
+#This file is created so that we can use our code afterwards like a "library"
+#If in any folder __init__.py is found then it is considered as package eg. in setup folder
+
+from setuptools import find_packages,setup
+
+
+ 
+ 
