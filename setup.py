8494c499a9a97d72946936966fce72186b3b4d4b
Saurabh-3089 <saurabhchauhan3089@gmail.com>
2023-03-03 10:04:33 +0530

    working on setup.py file

--- setup.py
+++ setup.py
@@ -2,6 +2,29 @@
 #If in any folder __init__.py is found then it is considered as package eg. in setup folder
 
 from setuptools import find_packages,setup
+from typing import List
+
+REQUIREMENT_FILE_NAME="requirements.txt"
+def get_requirements()->List[str]:
+    with open(REQUIREMENT_FILE_NAME) as requirement_files:
+        requirement_list = requirement_files.readlines()
+        requirement_list = [requirement_name.replace("\n","") for requirement_name in requirement_list]
+
+    if HYPHEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPHEN_E_DOT)
+    return requirement_list
+    
+
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="saurabh_chauhan",
+    author_email="saurabhchauhan1089@gmail.com",
+    packages = find_packages(),
+    install_requirements=get_requirements(),
+
+)
 
 
  
